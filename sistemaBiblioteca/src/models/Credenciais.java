373e2d5e3243b830e8659b47d8ac7b1e6007d0ad
Your Name <tiago_antunes8@hotmail.com>
2024-06-16 18:33:43 -0300

    javadoc do models

--- sistemaBiblioteca/src/models/Credenciais.java
+++ sistemaBiblioteca/src/models/Credenciais.java
@@ -7,10 +7,25 @@ public class Credenciais {
     private String tipo;
     private int id;
 
+    /**
+     * Construtor para criar credenciais com nome e tipo.
+     * 
+     * @param nome O nome do usuário.
+     * @param tipo O tipo de credencial.
+     */
     public Credenciais(String nome, String tipo) {
-    	this(nome, tipo, "", "", 0);
+        this(nome, tipo, "", "", 0);
     }
 
+    /**
+     * Construtor para criar credenciais com todos os detalhes.
+     * 
+     * @param login O login do usuário.
+     * @param senha A senha do usuário.
+     * @param nome  O nome do usuário.
+     * @param tipo  O tipo de credencial.
+     * @param id    O identificador do usuário.
+     */
     public Credenciais(String login, String senha, String nome, String tipo, int id) {
         this.login = login;
         this.senha = senha;
@@ -19,38 +34,83 @@ public class Credenciais {
         this.id = id;
     }
 
+    /**
+     * Obtém o login do usuário.
+     * 
+     * @return O login do usuário.
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * Define o login do usuário.
+     * 
+     * @param login O novo login do usuário.
+     */
     public void setLogin(String login) {
         this.login = login;
     }
 
+    /**
+     * Obtém a senha do usuário.
+     * 
+     * @return A senha do usuário.
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * Define a senha do usuário.
+     * 
+     * @param senha A nova senha do usuário.
+     */
     public void setSenha(String senha) {
         this.senha = senha;
     }
 
+    /**
+     * Obtém o nome do usuário.
+     * 
+     * @return O nome do usuário.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Define o nome do usuário.
+     * 
+     * @param nome O novo nome do usuário.
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Obtém o tipo de credencial do usuário.
+     * 
+     * @return O tipo de credencial do usuário.
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Define o tipo de credencial do usuário.
+     * 
+     * @param tipo O novo tipo de credencial do usuário.
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Obtém o identificador do usuário.
+     * 
+     * @return O identificador do usuário.
+     */
     public int getId() {
         return id;
     }
--- sistemaBiblioteca/src/models/Livro.java
+++ sistemaBiblioteca/src/models/Livro.java
@@ -8,6 +8,15 @@ public class Livro {
     private String categoria;
     private String quantidade;
 
+    /**
+     * Construtor da classe Livro.
+     * 
+     * @param titulo     O título do livro.
+     * @param autor      O autor do livro.
+     * @param ISBN       O ISBN do livro.
+     * @param categoria  A categoria do livro.
+     * @param quantidade A quantidade de exemplares do livro.
+     */
     public Livro(String titulo, String autor, String ISBN, String categoria, String quantidade) {
         this.titulo = titulo;
         this.autor = autor;
@@ -16,50 +25,110 @@ public class Livro {
         this.quantidade = quantidade;
     }
 
+    /**
+     * Obtém o ID do livro.
+     * 
+     * @return O ID do livro.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Define o ID do livro.
+     * 
+     * @param id O ID do livro.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Obtém o título do livro.
+     * 
+     * @return O título do livro.
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Define o título do livro.
+     * 
+     * @param titulo O título do livro.
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
+    /**
+     * Obtém o autor do livro.
+     * 
+     * @return O autor do livro.
+     */
     public String getAutor() {
         return autor;
     }
 
+    /**
+     * Define o autor do livro.
+     * 
+     * @param autor O autor do livro.
+     */
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
+    /**
+     * Obtém o ISBN do livro.
+     * 
+     * @return O ISBN do livro.
+     */
     public String getISBN() {
         return ISBN;
     }
 
+    /**
+     * Define o ISBN do livro.
+     * 
+     * @param ISBN O ISBN do livro.
+     */
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
 
+    /**
+     * Obtém a categoria do livro.
+     * 
+     * @return A categoria do livro.
+     */
     public String getCategoria() {
         return categoria;
     }
 
+    /**
+     * Define a categoria do livro.
+     * 
+     * @param categoria A categoria do livro.
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     * Obtém a quantidade de exemplares do livro.
+     * 
+     * @return A quantidade de exemplares do livro.
+     */
     public String getQuantidade() {
         return quantidade;
     }
 
+    /**
+     * Define a quantidade de exemplares do livro.
+     * 
+     * @param quantidade A quantidade de exemplares do livro.
+     */
     public void setQuantidade(String quantidade) {
         this.quantidade = quantidade;
     }
